355ec639a9b460ee10152fa1cddad669cf94f8d3
Michael Beutler <michael.beutler@iperka.com>
2022-04-20 21:04:51 +0200

    Added Google Analytics and AdSense. 🧶

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,34 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script id="ga-analytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+      `}
+      </Script>
+      <Script
+        id="Adsense-id"
+        data-ad-client={process.env.NEXT_PUBLIC_ADSENSE_CA}
+        async
+        strategy="beforeInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default appWithTranslation(MyApp);
